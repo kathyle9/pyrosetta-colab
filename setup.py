4680745a61759aa73c6487dd2fb0a598ab182234
kle16 <kle16@jhu.edu>
2019-08-27 13:21:19 -0400

    setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'pyrosetta-colab = pyrosetta-colab.__main__:main'
      ]
   },
-  version = '0.7',      # Start with a small number and increase it with every change you make
+  version = '0.8',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Contains commands for using PyRosetta in Google Colaboratory',   # Give a short description about your library
   author = 'Kathy Le',                   # Type in your name
