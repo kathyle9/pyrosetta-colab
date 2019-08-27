dcb004e59052f550f6e2b723aaaec3759583b878
kle16 <kle16@jhu.edu>
2019-08-27 11:58:59 -0400

    readme

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pyrosetta-colab',         # How you named your package folder (MyLib)
   packages = ['pyrosetta-colab'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Contains commands for using PyRosetta in Google Colaboratory',   # Give a short description about your library
   author = 'Kathy Le',                   # Type in your name
   author_email = 'kle16@jhu.edu',      # Type in your E-Mail
   url = 'https://github.com/kathyle9/pyrosetta-colab',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kathyle9/pyrosetta-colab/archive/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kathyle9/pyrosetta-colab/archive/v_03.tar.gz',    # I explain this later on
   keywords = ['PyRosetta', 'Google Colaboratory'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
